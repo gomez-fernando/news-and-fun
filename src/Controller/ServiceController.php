ab93e51451369290558fcaa6c5e5305caa985404
Fernando David Gómez Ortega <ferdgomez@hotmail.es>
2020-03-10 03:29:08 +0100

    creando el formulario de registro

---
+++ src/Controller/ServiceController.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+use App\Entity\Service;
+use App\Entity\User;
+
+class ServiceController extends AbstractController
+{
+    public function index()
+    {
+        $em = $this->getDoctrine()->getManager();
+
+        // prueba de entidades y relaciones
+//        $service_repo = $this->getDoctrine()->getRepository(Service::class);
+//        $services = $service_repo->findAll();
+//
+//        foreach ($services as $service){
+//            echo $service->getUser()->getName().'--->'.$service->getRssService().'<br>';
+//        }
+//        ----------------------------
+//            todos los usuarios y sus servicios
+        $user_repo = $this->getDoctrine()->getRepository(User::class);
+        $users = $user_repo->findAll();
+        foreach($users as $user){
+            echo "<h1>{$user->getName()} {$user->getSurname()}</h1>";
+
+            foreach ($user->getServices() as $service){
+                echo $service->getRssService().'<br>';
+            }
+        }
+
+        return $this->render('service/index.html.twig', [
+            'controller_name' => 'ServiceController',
+        ]);
+    }
+}
---
+++ src/Entity/Service.php
@@ -0,0 +1,107 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Service
+ *
+ * @ORM\Table(name="services", indexes={@ORM\Index(name="fk_service_user", columns={"user_id"})})
+ * @ORM\Entity
+ */
+class Service
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer", nullable=false)
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="IDENTITY")
+     */
+    private $id;
+
+    /**
+     * @var string|null
+     *
+     * @ORM\Column(name="rss_service", type="string", length=500, nullable=true)
+     */
+    private $rssService;
+
+    /**
+     * @var string|null
+     *
+     * @ORM\Column(name="description", type="string", length=520, nullable=true)
+     */
+    private $description;
+
+    /**
+     * @var \DateTime|null
+     *
+     * @ORM\Column(name="created_at", type="datetime", nullable=true)
+     */
+    private $createdAt;
+
+    /**
+     * @var \User
+     *
+     * @ORM\ManyToOne(targetEntity="App\Entity\User", inversedBy="tasks")
+     * @ORM\JoinColumns({
+     *   @ORM\JoinColumn(name="user_id", referencedColumnName="id")
+     * })
+     */
+    private $user;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getRssService(): ?string
+    {
+        return $this->rssService;
+    }
+
+    public function setRssService(?string $rssService): self
+    {
+        $this->rssService = $rssService;
+
+        return $this;
+    }
+
+    public function getDescription(): ?string
+    {
+        return $this->description;
+    }
+
+    public function setDescription(?string $description): self
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    public function getCreatedAt(): ?\DateTimeInterface
+    {
+        return $this->createdAt;
+    }
+
+    public function setCreatedAt(?\DateTimeInterface $createdAt): self
+    {
+        $this->createdAt = $createdAt;
+
+        return $this;
+    }
+
+    public function getUser(): ?User
+    {
+        return $this->user;
+    }
+
+    public function setUser(?Users $user): self
+    {
+        $this->user = $user;
+
+        return $this;
+    }
+}
