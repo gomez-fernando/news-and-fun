9c03c88470528a2109ccdf9ea6448ed91e2d984e
Fernando David Gómez Ortega <ferdgomez@hotmail.es>
2020-03-28 16:03:00 +0100

    funcionando correctamente

--- src/Form/ServiceType.php
+++ src/Form/ServiceType.php
@@ -9,11 +9,34 @@ use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use App\Entity\Category;
+use Symfony\Component\Security\Core\User\UserInterface;
 
 class ServiceType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
+        // global $kernel;
+
+        // if ('AppCache' == get_class($kernel)) {
+        //     $kernel = $kernel->getKernel();
+        // }
+        // $doctrine = $kernel->getContainer()->get('doctrine');
+
+        // $category_repo=$doctrine->getRepository(Category::class);
+        // $category = $category_repo->findBy(
+        //     ['user' => $user->getId()],
+        //     ['name' => 'ASC']
+        // );
+        // dd($category);
+
+        // $category_repo = $this->getDoctrine()->getRepository(Category::class);
+        // // $services = $service_repo->findAll();
+        // $categories = $category_repo->findBy(
+        //     ['user' => $user->getId()],
+        //     ['name' => 'ASC']
+        // );
+        // dd($categories);
+
         $builder->add('name', TextType::class, array(
             'label' => 'Nombre'
         ))
